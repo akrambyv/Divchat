cef15b9917bf2770c95ccc983fdbb5a3efe01a70
Əkrəm <abyvkrm2004@gmail.com>
2025-03-17 22:48:43 +0400

    initial commit

---
+++ src/database/entities/PostComment.entity.ts
@@ -0,0 +1,43 @@
+import { BaseEntity, Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { UserEntity } from "./User.entity";
+import { PostEntity } from "./Post.entity";
+
+@Entity('post_comments')
+export class PostCommentEntity extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    content: string;
+
+    @Column()
+    userId: number;
+
+    @Column()
+    postId: number;
+
+    @ManyToOne(() => UserEntity)
+    user: UserEntity;
+
+    @ManyToOne(() => PostEntity, (post) => post.comments, { onDelete: 'CASCADE' })
+    post: PostEntity;
+
+    @Column({ nullable: true })
+    replyId: number;
+
+    @ManyToOne(() => PostCommentEntity, (comment) => comment.replies)
+    replyTo: PostCommentEntity;
+
+    @OneToMany(() => PostCommentEntity, (comment) => comment.replyTo)
+    @JoinColumn({
+        name: 'replyId',
+        referencedColumnName: 'id',
+    })
+    replies: PostCommentEntity[];
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
